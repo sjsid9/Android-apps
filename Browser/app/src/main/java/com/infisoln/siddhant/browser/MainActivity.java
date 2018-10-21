6a52ece94b5007a6f0543c75fb30276c8d43c19d
sid <sjsid9@gmail.com>
2018-10-21 12:20:05 +0530

    Browser App Using WebView
    Signed-off-by: sid <sjsid9@gmail.com>

---
+++ Browser/app/src/main/java/com/infisoln/siddhant/browser/MainActivity.java
@@ -0,0 +1,51 @@
+package com.infisoln.siddhant.browser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        final WebView webView =findViewById(R.id.webView);
+        webView.loadUrl("https://www.google.com");
+        WebSettings webSettings =webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        final EditText editText = (EditText) findViewById(R.id.etUrl);
+
+        Button btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String url= editText.getText().toString();
+                Toast.makeText(MainActivity.this,url, Toast.LENGTH_SHORT).show();
+                webView.loadUrl("https://www."+url);
+            }
+        });
+
+        Intent intent = getIntent();
+
+        if(intent.getData()!=null){
+            Uri uri = intent.getData();
+            webView.loadUrl(uri.toString());
+        }
+
+
+    }
+}
