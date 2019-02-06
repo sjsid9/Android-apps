2d18783c732d8cc5f8fb8259eca8cf81bc5e980b
sid <sjsid9@gmail.com>
2019-02-07 00:49:00 +0530

    List view using Arraylist
    Signed-off-by: sid <sjsid9@gmail.com>

--- ListViews/app/src/main/java/com/infisoln/siddhant/listviews/MainActivity.java
+++ ListViews/app/src/main/java/com/infisoln/siddhant/listviews/MainActivity.java
@@ -2,11 +2,16 @@ package com.infisoln.siddhant.listviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] superheroes={"Captain America","Iron Man","Black Widow","SpiderMan",
-                "Aquaman","Goku","Shaktiman","Hulk","Batman","Wonder Woman",
-        "Vegeta","Picolo","gohan","Hawkeye","Scarlet Witch","Magneto","Flash","Superman","Professor X"};
+        final ArrayList<String> superheroes = new ArrayList<>(Arrays.asList("Captain America", "Iron Man", "Black Widow", "SpiderMan",
+                "Aquaman", "Goku", "Shaktiman", "Hulk", "Batman", "Wonder Woman",
+                "Vegeta", "Picolo", "gohan", "Hawkeye", "Scarlet Witch", "Magneto", "Flash", "Superman", "Professor X"));
+
+//        String[] superheroes={"Captain America","Iron Man","Black Widow","SpiderMan",
+//                "Aquaman","Goku","Shaktiman","Hulk","Batman","Wonder Woman",
+//        "Vegeta","Picolo","gohan","Hawkeye","Scarlet Witch","Magneto","Flash","Superman","Professor X"};
 
         ListView lvsuperheroes = findViewById(R.id.lvSuperheroes);
 
@@ -28,5 +37,18 @@ public class MainActivity extends AppCompatActivity {
 
         lvsuperheroes.setAdapter(arrayAdapter);
 
+
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newSuperHero = ((EditText)findViewById(R.id.etSuperheroName)).getText().toString();
+                Log.d("TAG",""+superheroes.size());
+                superheroes.add(newSuperHero);
+                Log.d("TAG",""+superheroes.size());
+            }
+        });
+
+        arrayAdapter.notifyDataSetChanged();
     }
 }
