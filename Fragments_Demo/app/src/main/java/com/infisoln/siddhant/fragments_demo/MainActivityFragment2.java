d05a0db107a74286b85e866dcb65a05de5363108
sid <sjsid9@gmail.com>
2019-02-12 00:48:21 +0530

    Fragments_Demo
    Signed-off-by: sid <sjsid9@gmail.com>

---
+++ Fragments_Demo/app/src/main/java/com/infisoln/siddhant/fragments_demo/MainActivityFragment2.java
@@ -0,0 +1,29 @@
+package com.infisoln.siddhant.fragments_demo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment2 extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_activity_fragment_two,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
