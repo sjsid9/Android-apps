32b6b94344e720e336ed50c3501c34ddd5d79aed
sid <sjsid9@gmail.com>
2019-02-17 01:41:58 +0530

    setting text using MainActivity instance on UI thread
    Signed-off-by: sid <sjsid9@gmail.com>

--- AsyncTask/app/src/main/java/com/infisoln/siddhant/asynctask/MainActivity.java
+++ AsyncTask/app/src/main/java/com/infisoln/siddhant/asynctask/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button1 = findViewById(R.id.btn);
+        final TextView textView = findViewById(R.id.tvTest);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,7 +23,15 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         for (long i = 0; i < 1000000000000L; i++) {
-
+                            if (i % 1000 == 0) {
+                                final long finalI1 = i;
+                                MainActivity.this.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        textView.setText(String.valueOf(finalI1));
+                                    }
+                                });
+                            }
                         }
                     }
                 }).start();
