c5b2e93dabc4fb15dd5ffc5421c36d6d8fe30344
sid <sjsid9@gmail.com>
2019-02-16 18:27:40 +0530

    Bottom Navigation Views
    Signed-off-by: sid <sjsid9@gmail.com>

--- Menus_Demo/app/src/main/java/com/infisoln/siddhant/menus_demo/MainActivity.java
+++ Menus_Demo/app/src/main/java/com/infisoln/siddhant/menus_demo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.infisoln.siddhant.menus_demo;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_about:
+                        Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_refresh:
+                        Toast.makeText(MainActivity.this, "Refresh", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_help:
+                        Toast.makeText(MainActivity.this, "Help", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_feedback:
+                        Toast.makeText(MainActivity.this, "Feedback", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 
     @Override
