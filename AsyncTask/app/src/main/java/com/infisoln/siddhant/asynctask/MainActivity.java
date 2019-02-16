4cc7fa1ce614b6553f5f31008b0186cec5a1a36a
sid <sjsid9@gmail.com>
2019-02-17 02:03:45 +0530

    setting count on textview (checking threshold of 16ms updation of frame)
    Signed-off-by: sid <sjsid9@gmail.com>

--- AsyncTask/app/src/main/java/com/infisoln/siddhant/asynctask/MainActivity.java
+++ AsyncTask/app/src/main/java/com/infisoln/siddhant/asynctask/MainActivity.java
@@ -2,6 +2,7 @@ package com.infisoln.siddhant.asynctask;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1 = findViewById(R.id.btn);
         final TextView textView = findViewById(R.id.tvTest);
+        textView.setText("Count starts");
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        for (long i = 0; i < 1000000000000L; i++) {
-                            if (i % 1000 == 0) {
+                        for (long i = 1; i < 10; i+=1) {
+                            if (i % 1 == 0) {
                                 final long finalI1 = i;
                                 MainActivity.this.runOnUiThread(new Runnable() {
                                     @Override
@@ -35,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 }).start();
-
-
+                Log.e("TAG", "onClick: counting done");
+                textView.setText("Counting done");
             }
         });
 
