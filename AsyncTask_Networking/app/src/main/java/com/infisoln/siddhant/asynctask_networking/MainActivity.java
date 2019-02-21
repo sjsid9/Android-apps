148a41abec56184125e58ebe0272eaec9ccb9f9d
sid <sjsid9@gmail.com>
2019-02-21 20:45:55 +0530

    Progress dialog Demo --> asyncTask Networking
    Signed-off-by: sid <sjsid9@gmail.com>

---
+++ AsyncTask_Networking/app/src/main/java/com/infisoln/siddhant/asynctask_networking/MainActivity.java
@@ -0,0 +1,52 @@
+package com.infisoln.siddhant.asynctask_networking;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyTask myTask = new MyTask();
+        myTask.execute("https://wwww.google.com");
+    }
+
+    ProgressDialog progressDialog;
+
+    class MyTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressDialog.cancel();
+        }
+
+    }
+
+}
