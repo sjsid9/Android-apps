33801895bdb276932fe6d3623288419cfda446df
sid <sjsid9@gmail.com>
2019-03-23 12:39:16 +0530

    start and stop dynamic broadcasts
    Signed-off-by: sid <sjsid9@gmail.com>

--- Broadcasts_Demo/app/src/main/java/com/infisoln/siddhant/broadcasts_demo/MainActivity.java
+++ Broadcasts_Demo/app/src/main/java/com/infisoln/siddhant/broadcasts_demo/MainActivity.java
@@ -2,25 +2,44 @@ package com.infisoln.siddhant.broadcasts_demo;
 
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyBroadcastReceiver mbr = new MyBroadcastReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnStart = findViewById(R.id.btnStart);
+        Button btnStop = findViewById(R.id.btnStop);
 
-        MyBroadcastReceiver mbr = new MyBroadcastReceiver();
 
-        IntentFilter inf = new IntentFilter();
-        inf.addAction(Intent.ACTION_POWER_CONNECTED);
-        inf.addAction(Intent.ACTION_POWER_DISCONNECTED);
-        inf.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentFilter inf = new IntentFilter();
+                inf.addAction(Intent.ACTION_POWER_CONNECTED);
+                inf.addAction(Intent.ACTION_POWER_DISCONNECTED);
+                inf.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
 
-        registerReceiver(mbr,inf);
+                registerReceiver(mbr, inf);
+
+            }
+        });
+
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unregisterReceiver(mbr);
+            }
+        });
 
     }
 }
